8f44e959d181985c572898749d8798bd9341ec67
Jayr Alencar <jayralencarpereira@gmail.com>
2016-04-19 11:40:00 -0300

    Creating controller in basel-crud

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Change Log
+This change log is started in 0.0.9;
+
+## 0.0.9 (2016-04-19)
+- Creating controller in basel-crud
---
+++ templates/controller.js
@@ -0,0 +1,49 @@
+"use strict";
+app.controller("CONTROLLER_NAME", function($scope, $location){
+
+	$scope.table_name = "TABLE_NAME";
+	$scope.primary_key = "PRIMARY_KEY";
+
+	//List
+	$scope.list = function(){
+		basel.database.runAsync("SELECT * FROM "+$scope.table_name, function(data){
+			$scope.items = data;
+		});
+	}
+
+	//Saving
+	$scope.save = function(){
+		if($scope.form[$scope.primary_key]){
+			//Edit
+			var id = $scope.form[$scope.primary_key];
+			delete $scope.form[$scope.primary_key];
+			delete $scope.form.$$hashKey; //Apaga elemento $$hashKey do objeto
+			basel.database.update($scope.table_name, $scope.form, {$scope.primary_key: id}); //entidade, dados, where
+		}else{
+			//new
+			basel.database.insert($scope.table_name, $scope.form); // entidade, dados
+		}
+		$scope.form = {};
+		$scope.list();
+		$('#modalNew').modal('hide');
+	}
+
+	// Cancel form
+	$scope.cancel = function(){
+		$scope.form = {};
+	}
+
+	//Abrindo para editar
+	$scope.edit = function(data){
+		$scope.form = dados;
+		$('#modalNew').modal('show');
+	}
+
+	//Excluindo
+	$scope.delete = function(data){
+		if(confirm("Are you sure?")){
+			basel.database.delete($scope.table_name, {$scope.primary_key: data[$scope.primary_key]});
+			$scope.list();
+		}
+	}
+});
